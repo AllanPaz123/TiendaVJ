e69aaf2fcc2b847c2a53f0f71d1d394f2e22e980
dixiefg04 <119626215+dixiefg04@users.noreply.github.com>
2025-07-20 16:31:23 +0000

    Carrito con Login

--- src/Controllers/Videojuegos/Carritos.php
+++ src/Controllers/Videojuegos/Carritos.php
@@ -2,14 +2,15 @@
 
 namespace Controllers\Videojuegos;
 
-use Controllers\PublicController;
+use Controllers\PrivateController;
 use Dao\Videojuegos\Carrito;
 use Views\Renderer;
 
-class Carritos extends PublicController
+class Carritos extends PrivateController
 {
     public function run(): void
     {
+        parent::__construct();
         $data["carritos"] = Carrito::getCarritos();
         Renderer::render("videojuegos/carritos", $data);
     }
